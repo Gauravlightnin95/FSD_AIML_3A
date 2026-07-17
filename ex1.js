bf6d5805cea3f318d3c9bb1d2db187bdbf3a8a4a
Gauravlightnin95 <gauravllege@gmail.com>
2026-07-17 14:20:16 +0530

    created ex1.js file

---
+++ ex1.js
@@ -0,0 +1,16 @@
+ //var a=10;
+// var a=20;
+// console.log("a=")
+// let a=10;
+// const b=25;
+// console.log("a=",a);
+// console.log("b=",b);
+// a=b+2;
+// console.log("a=",a);
+// function add(a,b){
+//     return a+b;
+// }
+// const sum=add(10,20);
+// console.log("sum=",sum);
+// const sums=(a,b)=>a+b;//lambda function
+// console.log("sums=",sums(10,20));
