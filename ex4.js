94aabf4246c4af7a386c66351d04555218c41bee
Gaurav <gauravllege@gmail.com>
2026-07-30 14:33:26 +0530

    Third Commit

--- ex4.js
+++ ex4.js
@@ -10,3 +10,6 @@ const {name,age,branch}=student;
 console.log("Name ",name)
 console.log("Age ",age)
 console.log("Branch ",branch)
+const num=[11,22,33]
+console.log(num[0]+num[1]+num[2])
+const [a,b,c]=num;// Square brackets for structure destructuring like on arrays and curly brackets for objects
