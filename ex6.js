583cf71341d64a61ba1e0326cc42f6c11498e95e
Gaurav <gauravllege@gmail.com>
2026-07-31 13:36:27 +0530

    31 july Commit

---
+++ ex6.js
@@ -0,0 +1,12 @@
+const sum=(...num)=>{
+    const s=num.reduce((s,i)=>(s+i));
+    return s;
+}
+export const add=(...num)=>{
+    let s=0;
+    for(let i of num){
+        s=s+i;
+    }
+    return s;
+}
+export {sum};
---
+++ test.js
@@ -0,0 +1,7 @@
+import {sum,add} from "./ex6.js"
+console.log("Sum= ",sum(1))
+console.log("Sum= ",sum(12,34))
+console.log("Sum= ",sum(10,20,60))
+console.log("Sum= ",sum(90,6,54))
+console.log("Add= ",add(10,20,60))
+console.log("Add= ",add(90,6,54))
